0bd0d75ec479035eede26c9d8d00d621922b2a82
mikaelbeat <ryynanenphm@gmail.com>
2026-06-08 14:45:15 +0300

    Update

--- tests/UIBasicstest.spec.js
+++ tests/UIBasicstest.spec.js
@@ -1,8 +1,20 @@
 
 const {test} = require('@playwright/test');
 
-test('FIrst Playwright test', function()
+test('Browser Context Playwright test', async ({browser})=>
+{
+    // These are optional, you can also use the default context and page provided by Playwright
+    // ({browser, page})=>
+    const context = await browser.newContext();
+    const page = await context.newPage();
+
+    await page.goto('https://www.rahulshettyacademy.com/AutomationPractice/');
+
+});
+
+test('Page Playwright test', async ({page})=>
 {
 
-    
+    await page.goto('https://google.com');
+
 });
