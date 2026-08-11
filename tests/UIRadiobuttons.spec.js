3d12b2b21ea87b072c1296e3c1a187c2c973c3de
mikaelbeat <ryynanenphm@gmail.com>
2026-08-11 17:16:12 +0300

    Update

---
+++ tests/UIRadiobuttons.spec.js
@@ -0,0 +1,23 @@
+
+const {test, expect} = require('@playwright/test');
+
+test('UI Control Test', async ({page})=>
+{
+    await page.goto('https://rahulshettyacademy.com/loginpagePractise/');
+    const username = page.locator("#username");
+    const password = page.locator("[type='password']");
+    const signInButton = page.locator("#signInBtn");
+
+    await page.locator(".radiotextsty").last().click();
+    await page.locator("#okayBtn").click();
+    await expect(page.locator(".radiotextsty").last()).toBeChecked();
+
+    const dropdown = page.locator("select.form-control");
+    await dropdown.selectOption("consult");
+
+    await page.locator("#terms").click();
+    await expect(page.locator("#terms")).toBeChecked();
+    await page.locator("#terms").uncheck();
+    await expect(page.locator("#terms")).not.toBeChecked();
+    //await page.pause();
+});
