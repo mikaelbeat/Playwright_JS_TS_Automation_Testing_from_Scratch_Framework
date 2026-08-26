8396faca92ae1ad3f6d8de6217a972c2527d8736
mikaelbeat <ryynanenphm@gmail.com>
2026-08-26 12:31:00 +0300

    Update

--- tests/End2End.spec.js
+++ tests/End2End.spec.js
@@ -43,6 +43,20 @@ test('Browser Context Playwright test', async ({page})=>
     await expect(await page.locator(".hero-primary")).toHaveText(" Thankyou for the order. ");
     const orderId = await page.locator(".em-spacer-1 .ng-star-inserted").textContent();
     console.log(orderId);
+    await page.locator("button[routerlink*='myorders']").click();
+    await page.locator("tbody").waitFor();
+    const rows = page.locator("tbody tr");
+
+    for(let i=0; i< await rows.count(); ++i){
+        const rowOrderId = await rows.nth(i).locator("th").textContent();
+        if(orderId.includes(rowOrderId)){
+            await rows.nth(i).locator("button").first().click();
+            break;
+        }
+    }
+
+    const orderIdDetails = await page.locator(".col-text").textContent();
+    expect(orderId.includes(orderIdDetails)).toBeTruthy();
 
 
     await page.pause();
