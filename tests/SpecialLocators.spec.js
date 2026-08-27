3745946004a70ef32a99be428b8605628935f6c2
mikaelbeat <ryynanenphm@gmail.com>
2026-08-27 16:32:39 +0300

    Update

--- tests/SpecialLocators.spec.js
+++ tests/SpecialLocators.spec.js
@@ -6,6 +6,12 @@ test('Special Locators', async ({ page }) => {
   await page.getByLabel("Check me out if you Love IceCreams!").check();
   await page.getByText("Employed").check();
   await page.getByLabel("Gender").selectOption("Female");
+  await page.getByPlaceholder("Password").fill("TosiSalainenJaPitkäSalainenSalasana");
+  await page.getByRole("button", { name: "Submit" }).click();
+  await expect(page.getByText("Success! The Form has been submitted successfully!")).toBeVisible();
+  await page.getByRole("link", { name: "Shop" }).click();
+  await page.locator("app-card").filter({ hasText: "Nokia Edge" }).getByRole("button", { name: "Add" }).click();
+
 
   await page.pause();
 
